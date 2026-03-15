929e728d8da05b78f5b462afedc377531f161fa4
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-03-15 17:44:29 +0530

    solving leetcode 181

---
+++ leetcode-sql-practice/leetcode-solutions.sql
@@ -0,0 +1,41 @@
+
+-- use leetcode_collections 
+
+-- Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+-- Truncate table Employee;
+-- insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+-- insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+-- insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL);
+-- insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL);
+
+
+-- leetcode 181
+
+-- Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int)
+-- Truncate table Employee
+-- insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+-- insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+-- insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL)
+-- insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL)
+
+-- in this question they have not given the manager table access 
+
+-- ohh i was thinking that manager is different table but it is the same ok so 
+-- we have manager access as wall as employee because manager is also employee
+
+-- Input: 
+-- Employee table:
+-- +----+-------+--------+-----------+
+-- | id | name  | salary | managerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | Null      |
+-- | 4  | Max   | 90000  | Null      |
+-- +----+-------+--------+-----------+
+-- Output: 
+-- +----------+
+-- | Employee |
+-- +----------+
+-- | Joe      |
+-- +----------+
--- leetcode-sql-practice/subqueries.sql
+++
@@ -1,7 +0,0 @@
--- Active: 1773518517561@@127.0.0.1@3306@leetcode_collections
-show tables ;
-CREATE TABLE Person (
-    personId INT PRIMARY KEY,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255)
-);
