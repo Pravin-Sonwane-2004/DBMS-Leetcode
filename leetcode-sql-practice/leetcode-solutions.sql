7e646a5a27393e65bdde0dead848c21ea2711a5c
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-03-16 18:46:24 +0530

    today i solved leetcde 181 and trying to solve another one

--- leetcode-sql-practice/leetcode-solutions.sql
+++ leetcode-sql-practice/leetcode-solutions.sql
@@ -131,3 +131,12 @@
 -- EXPLAIN SELECT * FROM orders WHERE user_id = 1;
 
 
+-- Create table If Not Exists Person (id int, email varchar(255));
+-- Truncate table Person;
+-- insert into Person (id, email) values ('1', 'a@b.com');
+-- insert into Person (id, email) values ('2', 'c@d.com');
+-- insert into Person (id, email) values ('3', 'a@b.com');
+
+
+
+-- select email from Person group by email having count(email) > 1
